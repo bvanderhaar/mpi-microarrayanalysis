b48f16f0f127819b38a2c50cb5600f375c304154
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-12-06 00:57:00 -0500

    add permutation analysis

--- cpu-microarrayanalysis.cpp
+++ cpu-microarrayanalysis.cpp
@@ -7,13 +7,11 @@
 #include <vector>
 #include "BVshared.h"
 
-
 struct gene_expression {
   std::string gene_name;
   std::vector<double> renal_disease;
   std::vector<double> control;
-}
-
+};
 
 int main(int argc, char *argv[]) {
   int current_row, current_col, rows = 4550, column_size = 69, i;
@@ -22,12 +20,10 @@ int main(int argc, char *argv[]) {
 
   // the first element is a string of the gene name
   std::vector<gene_expression> gene_data(rows);
-  for (current_row = 0; current_row < rows; current_row++) {
+  /*for (current_row = 0; current_row < rows; current_row++) {
     for (current_col = 0; current_col < column_size; current_col++) {
-      gene_data.push_back
     }
-  }
-
+  }*/
 
   current_row = 4549;
   gene_expression data_row;
@@ -45,25 +41,28 @@ int main(int argc, char *argv[]) {
 
   // combine vectors to do t-stat permutations
   std::vector<double> all_gene_data;
-  all_gene_data.reserve( data_row.renal_disease.size() + data_row.control.size() ); // preallocate memory
-  all_gene_data.insert( all_gene_data.end(), data_row.renal_disease.begin(), data_row.renal_disease.end() );
-  all_gene_data.insert( all_gene_data.end(), data_row.control.begin(), data_row.control.end() );
+  all_gene_data.reserve(data_row.renal_disease.size() +
+                        data_row.control.size()); // preallocate memory
+  all_gene_data.insert(all_gene_data.end(), data_row.renal_disease.begin(),
+                       data_row.renal_disease.end());
+  all_gene_data.insert(all_gene_data.end(), data_row.control.begin(),
+                       data_row.control.end());
   std::vector<double> permutation_t_stats;
   for (i = 0; i < 1000; i++) {
-    std::random_shuffle()
-    permutation_t_stats.push_back()
+    // std::random_shuffle() permutation_t_stats.push_back()
   }
 
   std::cout << "Renal Vector: ";
-  print_1d_vector(renal);
+  print_1d_vector(data_row.renal_disease);
   std::cout << "Normal Vector: ";
-  print_1d_vector(normal);
+  print_1d_vector(data_row.control);
   std::cout.precision(10);
-  std::cout << "Renal Standard Deviation: " << standard_deviation(renal)
-            << std::endl;
-  std::cout << "Normal Standard Deviation: " << standard_deviation(normal)
-            << std::endl;
-  std::cout << "Students T-Statistic: " << students_t_stat(renal, normal)
+  std::cout << "Renal Standard Deviation: "
+            << standard_deviation(data_row.renal_disease) << std::endl;
+  std::cout << "Normal Standard Deviation: "
+            << standard_deviation(data_row.control) << std::endl;
+  std::cout << "Students T-Statistic: "
+            << students_t_stat(data_row.renal_disease, data_row.control)
             << std::endl;
 
   return 0;
