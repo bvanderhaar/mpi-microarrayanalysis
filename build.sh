918d8dd23f71a12d1dd3eb4b170bb84c6f6bbfcc
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-12-06 22:41:27 -0500

    add compiler opts

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ if [ ! -f NCI-60.csv ]; then
 fi
 if [[ $OSTYPE == darwin* ]]; then
   clang-format -i cpu-microarrayanalysis.cpp mpi-microarrayanalysis.cpp BVshared.cpp BVshared.h BVgene.cpp BVgene.h
-  clang++ -std=c++14 cpu-microarrayanalysis.cpp BVshared.cpp BVgene.cpp -o cpu-microarrayanalysis
-  clang++ -std=c++14 BVSample.cpp BVshared.cpp BVgene.cpp -o sample
+  clang++ -std=c++14 -O2 cpu-microarrayanalysis.cpp BVshared.cpp BVgene.cpp -o cpu-microarrayanalysis
   mpic++ -std=c++14 mpi-microarrayanalysis.cpp BVshared.cpp -o mpi-microarrayanalysis
   chmod +x mpi-microarrayanalysis
   chmod +x cpu-microarrayanalysis
